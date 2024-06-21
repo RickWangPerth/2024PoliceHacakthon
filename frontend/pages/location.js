be68babf2a4cfda8e506ce073ec61ff294fe5fc3
FrankWANG <frankwang038@gmail.com>
2024-06-21 20:48:08 +1200

    updated api url

--- frontend/pages/location.js
+++ frontend/pages/location.js
@@ -12,7 +12,7 @@ const LocationSharing = () => {
 
                 // 使用环境变量中的后端URL
 
-          axios.post(`http://${window.location.host}:8000/api/location/`, { latitude, longitude })
+          axios.post(`http://${window.location.host}/api/location/`, { latitude, longitude })
             .then(response => {
               console.log('Location sent successfully:', response.data);
             })
