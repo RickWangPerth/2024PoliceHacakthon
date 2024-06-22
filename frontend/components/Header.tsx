a8039e99235475e4a8c8d0cadd0b89d1302fdf60
Xinlyu Wang <xinlalalele@gmail.com>
2024-06-22 20:23:20 +0800

    add axios & reportchat

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -3,10 +3,7 @@ import { Dialog, DialogPanel } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
-  { name: 'Dashboard', href: '/dashboard' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
+  { name: 'Dashboard', href: '/dashboard' }
 ]
 
 export default function Example() {
