efdfdf4f25492c15f661f3ea0df15c58235037de
Xinlyu Wang <xinlalalele@gmail.com>
2024-06-22 20:39:26 +0800

    add emergency page

---
+++ frontend/pages/emgerency.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import Chat from "@/components/chat";
+
+const Emergency = () => {
+
+  return (
+    <div className="h-full w-full">
+      <Chat />
+    </div>
+  );
+};
+
+export default Emergency;
