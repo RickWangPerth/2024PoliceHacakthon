f69caf8b72e2e6378f4350292a14ff6a04850716
FrankWANG <frankwang038@gmail.com>
2024-06-23 15:24:22 +1200

    updating reporter page

--- frontend/components/ReporterMap.tsx
+++ frontend/components/ReporterMap.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 interface ReporterMapProps {
-    caseId: number;
+    caseId: string;
 }
 
 const ReporterMap: React.FC<ReporterMapProps> = ({ caseId }) => {
